227a30f8dc805807e8e196f61278fca51952f081
luuzun <luuzun@naver.com>
2017-03-21 10:39:58 +0900

    임시변수 메서드로 전환 완료

--- src/Customer.java
+++ src/Customer.java
@@ -23,18 +23,30 @@ public class Customer {
 	}
 	
 	public String statement(){
-		double totalAmount = 0;//대여료
-		int frequentRenterPoints = 0;//적립포인트
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName()+"고객님의 대여기록\n");
+		
 		for(Rental each : rentals){
-			frequentRenterPoints += each.getFrequentRenterPoints();
-			
 			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
-			totalAmount += each.getCharge();
 		}//for loop
 		
-		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
+		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", getTotalCharge(), getFrequentRenterPoints()));
 		return sb.toString();
+	}
+
+	private int getFrequentRenterPoints() {
+		int frequentRenterPoints = 0;//적립포인트
+		for(Rental each : rentals){
+			frequentRenterPoints += each.getFrequentRenterPoints();
+		}
+		return frequentRenterPoints;
+	}
+
+	private double getTotalCharge() {
+		double result = 0.0;//대여료
+		for(Rental each : rentals){
+			result += each.getCharge();
+		}
+		return result;
 	}
 }
